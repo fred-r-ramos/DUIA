78c3dd51e19fae5bf83e027ff7843bd06bd602d6
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:31:54 +0100

    Update colombia overlayscript.R

--- STEP_1/Import_and_overlay/colombia overlayscript.R
+++ STEP_1/Import_and_overlay/colombia overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay colombia")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_co85 <- read_sf("geo2_co1985.shp")
 geo2_co93 <- read_sf("geo2_co1993.shp")
