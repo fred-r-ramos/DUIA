af84d3258ccc70e38c0b84a51fb728954cd28920
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:32:24 +0100

    Update egypt overlayscript.R

--- STEP_1/Import_and_overlay/egypt overlayscript.R
+++ STEP_1/Import_and_overlay/egypt overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay egypt")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_eg86 <- read_sf("geo2_eg1986.shp")
 geo2_eg96 <- read_sf("geo2_eg1996.shp")
