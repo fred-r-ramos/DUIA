ddead5eea61eb20d6d47cb35a0021672ddc402c9
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:46:14 +0100

    Update pakistan overlayscript.R

--- STEP_1/Import_and_overlay/pakistan overlayscript.R
+++ STEP_1/Import_and_overlay/pakistan overlayscript.R
@@ -3,8 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay pakistan")
 
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_pk81 <- read_sf("geo2_pk1981.shp")
 geo2_pk98 <- read_sf("geo2_pk1998.shp")
