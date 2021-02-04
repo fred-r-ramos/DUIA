24ffd093dd1199e4783dce9ab62d1aff2278b563
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:46:29 +0100

    Update philippines overlayscript.R

--- STEP_1/Import_and_overlay/philippines overlayscript.R
+++ STEP_1/Import_and_overlay/philippines overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay philippines")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_ph90 <- read_sf("geo2_ph1990.shp")
 geo2_ph00 <- read_sf("geo2_ph2000.shp")
