7fb03b0f58ace99ba18714baf742b631bece9d81
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:32:11 +0100

    Update ecuador overlayscript.R

--- STEP_1/Import_and_overlay/ecuador overlayscript.R
+++ STEP_1/Import_and_overlay/ecuador overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay ecuador")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_ec90 <- read_sf("geo2_ec1990.shp")
 geo2_ec01 <- read_sf("geo2_ec2001.shp")
