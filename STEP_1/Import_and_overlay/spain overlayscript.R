4bb5206fd82e8b787c0f2e3ad5af39291738162d
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:47:00 +0100

    Update spain overlayscript.R

--- STEP_1/Import_and_overlay/spain overlayscript.R
+++ STEP_1/Import_and_overlay/spain overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay spain")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_es91 <- read_sf("geo2_es1991.shp")
 geo2_es01 <- read_sf("geo2_es2001.shp")
