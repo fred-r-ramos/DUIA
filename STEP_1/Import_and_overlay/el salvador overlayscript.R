68b0d8e17188120ff96f98f7b9ced3b3c8940ce1
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:32:41 +0100

    Update el salvador overlayscript.R

--- STEP_1/Import_and_overlay/el salvador overlayscript.R
+++ STEP_1/Import_and_overlay/el salvador overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay el salvador")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_sv92 <- read_sf("geo2_sv1992.shp")
 geo2_sv07 <- read_sf("geo2_sv2007.shp")
