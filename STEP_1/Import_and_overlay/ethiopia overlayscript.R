62d2c6346d06ae03c548592768ef19a871675a8a
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:32:57 +0100

    Update ethiopia overlayscript.R

--- STEP_1/Import_and_overlay/ethiopia overlayscript.R
+++ STEP_1/Import_and_overlay/ethiopia overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay ethiopia")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_et84 <- read_sf("geo2_et1984.shp")
 geo2_et94 <- read_sf("geo2_et1994.shp")
