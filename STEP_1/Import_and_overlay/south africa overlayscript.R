c62986b1483bc33c50bafa91e07f31e2d63c1efd
Frederico Roman Ramos <fred.r.ramos@gmail.com>
2021-02-04 10:46:44 +0100

    Update south africa overlayscript.R

--- STEP_1/Import_and_overlay/south africa overlayscript.R
+++ STEP_1/Import_and_overlay/south africa overlayscript.R
@@ -3,7 +3,9 @@ library(tidyverse)
 library(dplyr)
 library(ipumsr)
 library(ggplot2)
-setwd("C:/Users/Gebruiker/Dropbox/Countriesoverlay David/overlay south africa")
+
+###set the working directory where all the dataset are located (IPUMS, Second level Administrative Shapefile, AUE Study area)
+setwd("   ")
 
 geo2_za96 <- read_sf("geo2_za1996.shp")
 geo2_za01 <- read_sf("geo2_za2001.shp")
